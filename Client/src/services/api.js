8e755edb76985310d2dc0a2bf38f32e484de0563
CodewithSaifii <hsaif317@gmail.com>
2025-08-01 02:37:40 -0700

    new

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL 
+});
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
